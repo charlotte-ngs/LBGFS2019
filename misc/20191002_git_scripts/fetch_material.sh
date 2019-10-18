dd51d33e60f6735d2ce0331bec16b6bfa2fa77da
charlotte-ngs <ngs.charlotte@gmail.com>
2019-10-18 10:24:23 +0200

    Fixed some further renaming bugs for breeding values

--- misc/20191002_git_scripts/fetch_material.sh
+++ misc/20191002_git_scripts/fetch_material.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 SCRIPTDIR=source/misctools/github
-DIRTOFETCH=ex/w04
+DIRTOFETCH=ex/w05
+# DIRTOFETCH=sol/w04
 
 #' fetch individual directory
 #' change to progdir
@@ -23,3 +24,10 @@ do
   sleep 2
 done
 
+# check content
+cat /home/quagadmin/user_admin/students-lbgfs2019/student-usernames-lbgfs2019 | while read s
+do 
+  echo " * Student: $s"
+  ls -l /home/$s/LBGFS2019/$DIRTOFETCH
+  sleep 2
+done
